ba0b0f282799abeb5f9a23ad1bbd2d938635e469
RostaJecna <levder@spsejecna.cz>
2024-04-11 10:45:10 +0200

    Rename `MainWindow` to `LoginWindow`

---
+++ Citizens360.WPF/LoginWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Citizens360.WPF;
+
+/// <summary>
+/// Interaction logic for LoginWindow.xaml
+/// </summary>
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
