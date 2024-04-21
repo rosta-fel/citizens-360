4727974d615bfe0f6f822b319e8b612ff61fc7a0
RostaJecna <levder@spsejecna.cz>
2024-04-21 17:01:36 +0200

    Add `Update` method for employee update, add possible nullable for IEnumerable employee

--- Citizens360.Application/Services/IEmployeeService.cs
+++ Citizens360.Application/Services/IEmployeeService.cs
@@ -5,7 +5,8 @@ namespace Citizens360.Application.Services;
 public interface IEmployeeService
 {
     Employee? Get(int id);
-    IEnumerable<Employee> Get();
+    IEnumerable<Employee?> Get();
     void Create(Employee employee);
     void Delete(int id);
+    void Update(Employee employee);
 }
