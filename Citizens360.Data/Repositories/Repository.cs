c740657bde9537e649084f4be1960d386875a458
RostaJecna <levder@spsejecna.cz>
2024-04-21 20:55:36 +0200

    Create `Repository` class with implemented `IRepository` interface

---
+++ Citizens360.Data/Repositories/Repository.cs
@@ -0,0 +1,36 @@
+using Citizens360.Domain.Interfaces.Entities;
+using Citizens360.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Citizens360.Data.Repositories;
+
+public class Repository<TEntity>(DbContext context) : IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    protected readonly DbContext Context = context;
+    
+    public void Create(TEntity entity)
+    {
+        Context.Set<TEntity>().Add(entity);
+    }
+
+    public IQueryable<TEntity> Get()
+    {
+        return Context.Set<TEntity>();
+    }
+
+    public TEntity? Get(int id)
+    {
+        return Context.Set<TEntity>().Find(id);
+    }
+
+    public void Update(TEntity entity)
+    {
+        Context.Set<TEntity>().Update(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+}
