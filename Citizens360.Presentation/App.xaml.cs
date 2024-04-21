6fbde69ffa5771c6b89c3f816e00c95e76b7dc68
RostaJecna <levder@spsejecna.cz>
2024-04-22 01:23:34 +0200

    Add app configuration builder, add loading connection string from configuration, register custom db context with connection string

--- Citizens360.Presentation/App.xaml.cs
+++ Citizens360.Presentation/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using Citizens360.Application.Services;
+using Citizens360.Data;
 using Citizens360.Presentation.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +15,21 @@ namespace Citizens360.Presentation;
 public partial class App : System.Windows.Application
 {
     private static readonly IHost Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
-        .ConfigureServices((_, services) =>
+        .ConfigureAppConfiguration((_, config) =>
         {
+            config.AddJsonFile("app-settings.json", optional: false, reloadOnChange: true);
+        })
+        .ConfigureServices((context, services) =>
+        {
+            string? connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<Citizens360Context>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+            
             services.AddSingleton<LoginWindow>();
             services.AddTransient<IEmployeeService, EmployeeService>();
-        }).Build();
+        })
+        .Build();
 
     protected override void OnStartup(StartupEventArgs e)
     {
