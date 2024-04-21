1587a6d74e7fceada2b1c07ce92ce97bb6615701
RostaJecna <levder@spsejecna.cz>
2024-04-21 19:07:03 +0200

    Implement test dependency injection

--- Citizens360.Presentation/App.xaml.cs
+++ Citizens360.Presentation/App.xaml.cs
@@ -1,10 +1,34 @@
 ﻿using System.Windows;
+using Citizens360.Application.Services;
+using Citizens360.Presentation.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Citizens360.Presentation;
 
 /// <summary>
-/// Interaction logic for App.xaml
+///     Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
+    private static readonly IHost Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+        .ConfigureServices((_, services) =>
+        {
+            services.AddTransient<IEmployeeService, EmployeeService>();
+        }).Build();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Host.Start();
+        Host.Services.GetRequiredService<LoginWindow>().Show();
+        
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Host.StopAsync().Wait();
+        
+        base.OnExit(e);
+    }
 }
