22e56d562d6416e9a0ce664eca48e7d98a887675
RostaJecna <levder@spsejecna.cz>
2024-04-21 23:01:23 +0200

    Implement `UoW` interface, implement methods, add `IEmployeeRepository` and make class as `Disposable`

---
+++ Citizens360.Data/UnitOfWork.cs
@@ -0,0 +1,39 @@
+using Citizens360.Data.Repositories;
+using Citizens360.Domain.Interfaces;
+using Citizens360.Domain.Interfaces.Repositories;
+
+namespace Citizens360.Data;
+
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    public IEmployeeRepository Employees { get; }
+
+    private readonly Citizens360Context _context;
+    private bool _disposed;
+
+    public UnitOfWork(Citizens360Context context)
+    {
+        _context = context;
+
+        Employees = new EmployeeRepository(_context);
+    }
+
+    public int Commit()
+    {
+        return _context.SaveChanges();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+            if (disposing)
+                _context.Dispose();
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
