b0769bc6ce48a9a3d2fee0cc3b853b8f513a61b2
RostaJecna <levder@spsejecna.cz>
2024-04-22 23:51:07 +0200

    Fix type typo for Username, add necessary attributes for properties

--- Citizens360.Domain/Entities/Employee.cs
+++ Citizens360.Domain/Entities/Employee.cs
@@ -1,12 +1,26 @@
+using System.ComponentModel.DataAnnotations;
 using Citizens360.Domain.Interfaces.Entities;
 
 namespace Citizens360.Domain.Entities;
 
 public class Employee : IEntity
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(32)]
     public string? FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(32)]
     public string? LastName { get; set; }
-    public int Username { get; set; }
+    
+    [Required]
+    [MaxLength(16)]
+    public string? Username { get; set; }
+    
+    [Required]
+    [MaxLength(64)]
     public string? Password { get; set; }
 }
