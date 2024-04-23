929df87620efe5a3a15a804e196fe5a2ae1b8cc3
RostaJecna <levder@spsejecna.cz>
2024-04-23 02:47:44 +0200

    Add services to DI container from business and data layers

--- Citizens360.Presentation/App.xaml.cs
+++ Citizens360.Presentation/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
+using Citizens360.Application.Services;
 using Citizens360.Data;
+using Citizens360.Domain.Interfaces;
 using Citizens360.Presentation.Configuration;
 using Citizens360.Presentation.Windows;
 using Microsoft.EntityFrameworkCore;
@@ -15,24 +17,24 @@ namespace Citizens360.Presentation;
 public partial class App
 {
     private static readonly IHost Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
-        .ConfigureAppConfiguration((_, config) =>
-        {
-            config.AddJsonFile(AppSettings.ConfigFileName, optional: false, reloadOnChange: true);
-        })
+        .ConfigureAppConfiguration((_, config) => { config.AddJsonFile(AppSettings.ConfigFileName, false, true); })
         .ConfigureServices((context, services) =>
         {
             services.AddSingleton<AppSettings>(_ =>
             {
                 IConfiguration configuration = context.Configuration;
-                return AppSettings.Load(configuration) ?? throw new InvalidOperationException("Failed to load app settings.");
+                return AppSettings.Load(configuration) ??
+                       throw new InvalidOperationException("Failed to load app settings.");
             });
-            
+
             string? connectionString = context.Configuration.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<Citizens360DbContext>(options =>
                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-            
+
             services.AddSingleton<LoginWindow>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IEmployeeService, EmployeeService>();
         })
         .Build();
 
@@ -40,14 +42,20 @@ public partial class App
     {
         Host.Start();
         Host.Services.GetRequiredService<LoginWindow>().Show();
-        
+
         base.OnStartup(e);
+    }
+
+    public static T GetRequiredService<T>()
+        where T : class
+    {
+        return Host.Services.GetRequiredService<T>();
     }
 
     protected override void OnExit(ExitEventArgs e)
     {
         Host.StopAsync().Wait();
-        
+
         base.OnExit(e);
     }
 }
